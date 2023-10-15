40f3a916254337dd84c3fcfc68476a38e10cf60a
FaustVX <jodu035@gmail.com>
2023-10-16 00:00:31 +0200

    Added mod script

---
+++ mod/data.lua
@@ -0,0 +1,9 @@
+if data.raw.technology["logistic-cargo-wagon"] then
+    data.raw.technology["logistic-cargo-wagon"].unit.ingredients =
+    {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"utility-science-pack", 1}
+    }
+end
